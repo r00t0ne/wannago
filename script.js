6aa9f1469a4819059ecc2f5aebfe9c39c5e11991
r00t0ne <aykhanazizli@proton.me>
2025-03-09 06:19:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function nextPage() {
+    window.location.href = "yes.html"; // Yes butonuna basınca yönlendirilecek sayfa
+}
+
+function moveButton() {
+    const button = document.getElementById("noButton");
+    const maxX = window.innerWidth - button.offsetWidth;
+    const maxY = window.innerHeight - button.offsetHeight;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    button.style.left = `${randomX}px`;
+    button.style.top = `${randomY}px`;
+}
+
